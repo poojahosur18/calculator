eb07e68f86136e02dc8c9f19c6b60864769acc5a
poojahosur18 <poojahosur18gmail.com>
2025-05-06 10:28:09 +0530

    ave

---
+++ DivisionOperation.java
@@ -0,0 +1,14 @@
+package calculator;
+
+public class DivisionOperation {
+    public static void main(String[] args) {
+        int a=12;
+        int b=0;
+        try{
+            int div=a/b;
+            System.out.println("division: "+div);
+        }catch(ArithmeticException e){
+            System.out.println("Error: division by zero not possible");
+ }
+}
+}
